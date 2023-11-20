5e1d747c58700ee82fb4458dad985efbbb1db390
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-11-20 22:33:30 +0100

    rst math to github math

--- Irrads_relation/E_ratio_script.py
+++ Irrads_relation/E_ratio_script.py
@@ -5,30 +5,32 @@ Script to bench the generator algorithm against known cutoff wavelength of
 Silicon-based PV cell technologies.
 """
 
+# %% Initialization
 import numpy as np
 
 from spectrl2_E_ratio_bench import MR_E_ratio
 from irrads_relation_fracs import LAMBDA0
 
-
+# Matrix of values to test
 # Atmosphere characterization required params
 N = 4
-# Wikipedia cites range 870 to 1085 hPa
-surface_pressure = np.linspace(870.0, 1085.0, N) * 100  # Pa
-# SPECTRL2 paper Fig 4-6: 1.0 to 2.5 cm
-precipitable_water = np.linspace(1.0, 2.5, N)
-# SPECTRL2 paper Fig 4-5: 0.08 to 0.30 [unitless]
-aerosol_turbidity_500nm = np.linspace(0.08, 0.30, N)
-
+spectrl2_generator_input = {
+    # Wikipedia cites range 870 to 1085 hPa
+    "surface_pressure": np.linspace(870.0, 1085.0, N) * 100,  # Pa
+    # SPECTRL2 paper Fig 4-6: 1.0 to 2.5 cm
+    "precipitable_water": np.linspace(1.0, 2.5, N),
+    # SPECTRL2 paper Fig 4-5: 0.08 to 0.30 [unitless]
+    "aerosol_turbidity_500nm": np.linspace(0.08, 0.30, N),
+}
 
 bench = MR_E_ratio()  # default values for a start
 
-# Test for each cutoff wavelength we consider
-for unique_cutoff_lambda in set(LAMBDA0.values()):
-    bench.cutoff_lambda = unique_cutoff_lambda
-    # Preliminarily test SPECTRL2 required params
-    bench.simulate_from_product(
-        surface_pressure=surface_pressure,
-        precipitable_water=precipitable_water,
-        aerosol_turbidity_500nm=aerosol_turbidity_500nm,
-    )
+# %%
+# Test with monosi/polysi cutoff wavelength
+bench.cutoff_lambda = LAMBDA0["monosi"]  # == polysi
+bench.simulate_from_product(**spectrl2_generator_input)
+
+# %%
+# Test with asi cutoff wavelength
+bench.cutoff_lambda = LAMBDA0["asi"]
+bench.simulate_from_product(**spectrl2_generator_input)
--- README.rst
+++ README.rst
@@ -4,14 +4,15 @@ Modelling and characterization of usable irradiance
 To be used with N. Martín and J. M. Ruiz spectral mismatch factor. From Nuria's thesis
 3.2b equation:
 
-.. math::
+$$
 
     PS = 1 - \frac{S_{efE(\lambda)}}{S_{ef\bar{G}(\lambda)}}
         \frac{E_{\lambda<\lambda_0}}{\bar{G}_{\lambda<\lambda_0}}
         \frac{\bar{G}}{E}
+$$
 
 In this repo, I will be working in a first modelling of
-:math:`\frac{E_{\lambda<\lambda_0}{E}`.
+$\frac{E_{\lambda<\lambda_0}{E}$.
 
 
 Available workflows
