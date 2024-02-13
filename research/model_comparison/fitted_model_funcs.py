cc8ead982b22f209e5ff5208b72244efdb084a15
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-02-14 00:28:24 +0100

    Add fitted models

--- research/model_comparison/fitted_model_funcs.py
+++ research/model_comparison/fitted_model_funcs.py
@@ -11,17 +11,23 @@ def mr_alike_inst(
 ):
     # components
     IRRAD_COMPONENTS = (
-        "poa_global",  # "poa_direct", # "poa_sky_diffuse", # "poa_ground_diffuse",
+        "poa_global", "poa_direct", "poa_sky_diffuse", "poa_ground_diffuse",
     )
 
     ECHEDEY_PARAMS = pd.DataFrame(
-        index=pd.MultiIndex.from_product([(1100.0, 800.0), IRRAD_COMPONENTS]),
+        index=pd.MultiIndex.from_product([(800.0, 1100.0), IRRAD_COMPONENTS]),
         columns=("c", "a", "b"),
-        data=[  # [c | a | b]
-            # λ₀ = 1100nm (monosi/polysi)
-            [0.72379846, -0.06487852, -0.02485946],  # Global
+        data=[  # [ c | a | b ]
             # λ₀ = 800nm (asi)
-            [0.45153287, 0.21800605, -0.0138813],  # Global
+            [0.44529369, 0.149695, -0.01518124],  # poa_global
+            [4.72919551e-20, .00000354e+00, .00000000e+00],  # poa_direct
+            [0.96450068, .15149651, .01127807],  # poa_sky_diffuse
+            [4.74678806e-01, 6.12130156e-01, 1.97556457e-04],  # poa_ground_diffuse
+            # λ₀ = 1100nm (monosi/polysi)
+            [0.72195231, 0.07684853, 0.02505114],  # poa_global
+            [1.99765180e-16, .00670533e+00, .00000395e+00],  # poa_direct
+            [1.02297009, .43289747, .00263201],  # poa_sky_diffuse
+            [0.76003905, 0.10538212, 0.00317642],  # poa_ground_diffuse
         ],
     )
 
@@ -42,11 +48,9 @@ def mr_alike_inst(
     kt_delta = clearness_index - 0.74
     am_delta = airmass_absolute - 1.5
 
-    # Output values initialization depending on scalar/vector input
-    if np.isscalar(clearness_index) and np.isscalar(airmass_absolute):
-        modifiers = dict(zip(IRRAD_COMPONENTS, (np.nan,) * 3))
-    else:
-        modifiers = pd.DataFrame(columns=IRRAD_COMPONENTS)
+    # Output values
+    modifiers = pd.DataFrame(columns=IRRAD_COMPONENTS)
+
     # Calculate mismatch modifier for each irradiation
     for irrad_type in component_select:
         _coeffs = _params.loc[irrad_type]
